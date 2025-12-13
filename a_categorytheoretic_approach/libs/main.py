93e3e6e8207d1a89e90e1d804dfe19e55769508b
felekekinfe <felekekinfe@gmail.com>
2025-12-13 19:28:46 +0000

    add modification

--- a_categorytheoretic_approach/libs/main.py
+++ a_categorytheoretic_approach/libs/main.py
@@ -4,7 +4,7 @@ from hyperon.ext import register_atoms
 from .agents import context_preprocessing_agent,prompt_agent
 
 # Configuration
-AGENTS = ["context_preprocessing","algspec_builder","generalization_helper","amalgam_builder"]
+AGENTS = ["context-preprocessing","algspec-builder","generalization-helper","amalgam-builder"]
 
 @register_atoms(pass_metta=True)
 def grounded_atoms(metta):
@@ -12,30 +12,30 @@ def grounded_atoms(metta):
     registered_operations = {}
 
     for agent in AGENTS:
-        operation_name = f"gpt_{agent}"  # e.g., gpt_algspec_builder
+        operation_name = f"gpt-{agent}"  # e.g., gpt-algspec-builder
 
-        if agent == "context_preprocessing":
+        if agent == "context-preprocessing":
             registered_operations[operation_name] = OperationAtom(
                 operation_name,
                 lambda *args: context_preprocessing_agent(metta, *args),
                 [AtomType.ATOM, AtomType.ATOM, AtomType.ATOM, AtomType.ATOM, "Expression"],
                 unwrap=False
             )
-        elif agent == "algspec_builder":
+        elif agent == "algspec-builder":
             registered_operations[operation_name] = OperationAtom(
             operation_name,
             lambda *args, agent=agent: prompt_agent(metta, agent, *args),
             [AtomType.ATOM, AtomType.ATOM, AtomType.ATOM, "Expression"],
             unwrap=False
         )
-        elif agent == "generalization_helper":
+        elif agent == "generalization-helper":
             registered_operations[operation_name] = OperationAtom(
                 operation_name,
                 lambda *args, agent=agent: prompt_agent(metta, agent, *args),
                 [AtomType.ATOM, AtomType.ATOM, "Expression"],
                 unwrap=False
             )
-        elif agent == "amalgam_builder":
+        elif agent == "amalgam-builder":
             registered_operations[operation_name] = OperationAtom(
                 operation_name,
                 lambda *args, agent=agent: prompt_agent(metta, agent, *args),
